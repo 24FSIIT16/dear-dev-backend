215673893b626f28ff926e1151025ed6cc4b4ea5
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-07-05 16:25:30 +0200

    DEAR-71: fix application properties

--- src/main/java/ch/fhnw/deardevbackend/controller/UserController.java
+++ src/main/java/ch/fhnw/deardevbackend/controller/UserController.java
@@ -4,18 +4,22 @@ import ch.fhnw.deardevbackend.entities.User;
 import ch.fhnw.deardevbackend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("v1/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/")
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("")
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok().body(userService.getAllUsers());
     }
