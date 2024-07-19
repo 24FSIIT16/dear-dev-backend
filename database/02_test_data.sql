21ccb2cfc49e9a1b227d2a642d8c8fb4038c4bc7
petra kohler <smuefsmuef@gmail.com>
2024-07-19 17:19:56 +0200

    DEAR-120 set userids testdata

--- database/02_test_data.sql
+++ database/02_test_data.sql
@@ -1,16 +1,16 @@
 INSERT INTO public.users (id, name, email, "emailVerified", image, username)
-VALUES (1, 'Hans Müller', 'hans@test.com', null, null, 'Hansi'),
-       (2, 'Berta Roberts', 'berti@test.com', null, null, 'Berti');
+VALUES (100001, 'Hans Müller', 'hans@test.com', null, null, 'Hansi'),
+       (100002, 'Berta Roberts', 'berti@test.com', null, null, 'Berti');
 
 INSERT INTO public.team (id, name, current_sprint_id, config_id, code, created_by, created_at, active)
-VALUES (1, 'Team Alpha', null, null, 'ALF1', 1, '2024-07-13 15:19:11.164000 +00:00', true);
+VALUES (1, 'Team Alpha', null, null, 'ALF1', 100001, '2024-07-13 15:19:11.164000 +00:00', true);
 INSERT INTO public.team (id, name, current_sprint_id, config_id, code, created_by, created_at, active)
-VALUES (2, 'Team Beta', null, null, 'BET2', 1, '2024-07-13 15:20:22.162000 +00:00', true);
+VALUES (2, 'Team Beta', null, null, 'BET2', 100001, '2024-07-13 15:20:22.162000 +00:00', true);
 
 INSERT INTO public.team_member (id, user_id, team_id, role, active)
-VALUES (1, 1, 1, 'ADMIN', true);
+VALUES (1, 100001, 1, 'ADMIN', true);
 INSERT INTO public.team_member (id, user_id, team_id, role, active)
-VALUES (2, 2, 2, 'MEMBER', true);
+VALUES (2, 100002, 2, 'MEMBER', true);
 
 INSERT INTO public.team_config (id, work_kinds)
 VALUES (1, '{CODING,MEETING}');
@@ -33,13 +33,13 @@ VALUES ('Relaxed'),
        ('Exhausted'),
        ('Sick');
 INSERT INTO happiness_survey (user_id, score)
-VALUES (1, 4),
-       (1, 2),
-       (1, 3),
-       (2, 1);
+VALUES (100001, 4),
+       (100001, 2),
+       (100001, 3),
+       (100002, 1);
 INSERT INTO work_kind_survey (user_id, score, work_kind_id)
-VALUES (1, 6, 1),
-       (2, 7, 2);
+VALUES (100001, 6, 1),
+       (100002, 7, 2);
 INSERT INTO emotion_survey (user_id, score, emotion_id)
-VALUES (1, 9, 1),
-       (2, 5, 2);
+VALUES (100001, 9, 1),
+       (100002, 5, 2);
