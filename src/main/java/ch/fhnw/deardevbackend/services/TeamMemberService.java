bf97a7e868075a5e60107c23160fea4a6f5729af
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-07-21 20:27:53 +0200

    DEAR-125: add securityUtil to get current user

--- src/main/java/ch/fhnw/deardevbackend/services/TeamMemberService.java
+++ src/main/java/ch/fhnw/deardevbackend/services/TeamMemberService.java
@@ -1,12 +1,12 @@
 package ch.fhnw.deardevbackend.services;
 
 import ch.fhnw.deardevbackend.dto.TeamWithMembersDTO;
-import ch.fhnw.deardevbackend.entities.Role;
 import ch.fhnw.deardevbackend.entities.Team;
 import ch.fhnw.deardevbackend.entities.TeamMemberWithUser;
 import ch.fhnw.deardevbackend.mapper.TeamWithMembersMapper;
 import ch.fhnw.deardevbackend.repositories.TeamMemberWithUserRepository;
 import ch.fhnw.deardevbackend.repositories.TeamRepository;
+import ch.fhnw.deardevbackend.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,8 +27,9 @@ public class TeamMemberService {
     public TeamWithMembersDTO getTeamMembersByTeamId(Integer teamId) {
         List<TeamMemberWithUser> members = teamMemberWithUserRepository.findByTeamId(teamId);
         Team team = teamRepository.findById(teamId).orElseThrow(() -> new RuntimeException("Team not found with id: " + teamId));
-        Integer adminId = members.stream().filter(member -> member.getRole().equals(Role.ADMIN)).toList().getFirst().getUser().getId();
-        boolean isActive = adminId.equals(team.getCreatedBy());
-        return teamWithMembersMapper.toDTO(team, members, isActive);
+
+        boolean isAdmin = team.getCreatedBy().equals(SecurityUtil.getCurrentUserId());
+
+        return teamWithMembersMapper.toDTO(team, members, isAdmin);
     }
 }
