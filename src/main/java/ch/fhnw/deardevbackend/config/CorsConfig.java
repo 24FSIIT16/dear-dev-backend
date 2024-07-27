4743fe7a6adda5d88e4376cc30752448205fee6e
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-07-27 10:32:34 +0200

    Update CorsConfig

--- src/main/java/ch/fhnw/deardevbackend/config/CorsConfig.java
+++ src/main/java/ch/fhnw/deardevbackend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "http://86.119.49.89")
                         .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
